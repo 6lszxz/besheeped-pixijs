e9b12eb631d2d4c4326549018a91ff2ee2ce34dd
Xingxinyuxxy <114076900+Xingxinyuxxy@users.noreply.github.com>
2022-11-10 20:45:22 +0800

    Update structures.js

--- src/scripts/structures.js
+++ src/scripts/structures.js
@@ -3,8 +3,9 @@ import cow from '../assets/structures/cow.png'
 import chicken from '../assets/structures/chicken.png'
 import mayonnaiseMachine from '../assets/structures/mayonnaiseMachine.png'
 import loom from '../assets/structures/loom.png'
+import smileEggPress from '../assets/structures/smileEggPress.png'
 
 export{
-    sheep, cow, chicken, mayonnaiseMachine, loom,
+    sheep, cow, chicken, mayonnaiseMachine, loom, smileEggPress,
     
 }
