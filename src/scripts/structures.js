3cc03029661143596c9c0f67a8e6e52fd7a205c2
Xingxinyuxxy <114076900+Xingxinyuxxy@users.noreply.github.com>
2022-11-11 10:12:05 +0800

    Update structures.js

--- src/scripts/structures.js
+++ src/scripts/structures.js
@@ -6,7 +6,7 @@ import pig from '../assets/structures/pig.png'
 import mayonnaiseMachine from '../assets/structures/mayonnaiseMachine.png'
 import loom from '../assets/structures/loom.png'
 import smileEggPress from '../assets/structures/smileEggPress.png'
-
+import oilmaker from '../assets/structures/oil_maker.png'
 export{
     sheep, cow, chicken, mayonnaiseMachine, loom, smileEggPress,
     
