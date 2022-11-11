076b2de6bd083d20e9f9dfa63537ffc5fd3f8aa7
Xingxinyuxxy <114076900+Xingxinyuxxy@users.noreply.github.com>
2022-11-11 09:02:19 +0800

    Update structures.js

--- src/scripts/structures.js
+++ src/scripts/structures.js
@@ -1,6 +1,7 @@
 import sheep from '../assets/structures/sheep.png'
 import cow from '../assets/structures/cow.png'
 import chicken from '../assets/structures/chicken.png'
+import rabbit from '../assets/structures/Rabbit.png'
 import mayonnaiseMachine from '../assets/structures/mayonnaiseMachine.png'
 import loom from '../assets/structures/loom.png'
 import smileEggPress from '../assets/structures/smileEggPress.png'
