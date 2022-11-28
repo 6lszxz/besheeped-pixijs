337ff6650588c8b56e21bdff00e4de3afc090a01
lzj26 <113963202+lzj26@users.noreply.github.com>
2022-11-28 18:51:08 +0800

    在scripts新增randomEvent,js

---
+++ src/scripts/randomEvent.js
@@ -0,0 +1,6 @@
+import random01 from '../assets/randomEvent/胡萝卜贼.png';
+import random02 from '../assets/randomEvent/破坏财物.png';
+import random03 from '../assets/randomEvent/寒潮.png';
+import random04 from '../assets/randomEvent/动物心情好.png';
+import random05 from '../assets/randomEvent/收购商请喝茶.png';
+export {random01,random02,random03,random04,random05}
